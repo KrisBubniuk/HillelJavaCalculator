70a79690f5bc75ef1553efcd40557f4e739d20b6
KrisBubniuk <bubniuk77744@gmail.com>
2023-05-10 16:59:15 +0300

    calculatorcommit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter symbol (+, -, *, /): ");
+        char symbol = scanner.next().charAt(0);
+
+        int result = 0;
+        switch (symbol) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                return;
+        }
+        System.out.println(num1 + " " + symbol + " " + num2 + " = " + result);
+    }
+}
